34892b3ea4fb3543c0af9aa0cd3ba9698e021cf1
jolufergo <jolufergo@gmail.com>
2013-06-15 02:01:01 +0200

    primer commit

---
+++ src/Bienvenido1.java
@@ -0,0 +1,12 @@
+
+public class Bienvenido1 { //esta es la clase llamada Bienvenido
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) { //este es el metodo llamado main
+		// TODO Auto-generated method stub
+		System.out.println( "Bienvenido a la programacion en Java!" );
+	}
+
+}
